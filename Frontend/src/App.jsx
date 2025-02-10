a64502ecd7ce1f7c5a50f0adf8e257c63b3e121e
Jayash Bhuyar <jayashbhuyar@gmail.com>
2025-02-10 14:22:38 +0530

    .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -47,7 +47,7 @@ import DetectButton from './Components/User/DetectButton'
 import DailyCalories from "./Health Tracker/DailyCalories";
 import Exercisecalories from "./Health Tracker/ExerciseCalories";
 import FoodSearch from "./Health Tracker/FoodSearch";
-import InstantFoodDetails from "./Health Tracker/InstantFoodDetails";
+import InstantFoodDetails from "./Health Tracker/instantFoodDetails";
 
 function App() {
   return (
